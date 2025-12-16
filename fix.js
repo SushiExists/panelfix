b38fd657ddf86dd661170a5dc56c0182c1fc4a49
SushiExists <94861355+SushiExists@users.noreply.github.com>
2025-12-16 01:29:08 -0800

    Combination

--- fix.js
+++ fix.js
@@ -1,10 +1,3 @@
-window.alert("Testing");
-
-window.addEventListener ('DOMContentLoaded', () => { 
-  document.querySelectorAll('img').forEach(img => { 
-    if (img.src.includes('//k') && img.src.includes('.mb')) { 
-      img.referrerPolicy = "no-referrer"; 
-      img.src = img.src.replace('//k', '//n'); 
-    } 
-  }); 
-}
+window.addEventListener ('DOMContentLoaded', () => {
+setInterval(() => { document.querySelectorAll('img').forEach(img => { if (img.src.includes('//k') && img.src.includes('.mb')) { img.referrerPolicy = "no-referrer"; img.src = img.src.replace('//k', '//n'); } }); }, 2000);
+});
