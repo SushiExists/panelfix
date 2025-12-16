4054f0af2caf3107a131b00e729387d78dcca9c6
SushiExists <94861355+SushiExists@users.noreply.github.com>
2025-12-16 00:44:44 -0800

    added code
    Add interval function to update image sources

--- fix.js
+++ fix.js
@@ -1,1 +1,8 @@
-
+setInterval(() => { 
+  document.querySelectorAll('img').forEach(img => { 
+    if (img.src.includes('//k') && img.src.includes('.mb')) { 
+      img.referrerPolicy = "no-referrer"; 
+      img.src = img.src.replace('//k', '//n'); 
+    } 
+  }); 
+}, 2000);
